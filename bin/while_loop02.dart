1369d2204bfc25be59ad047c7d9e6726dc05bfd8
G'ayrat <shirnboyevgayrat@gmail.com>
2022-10-26 12:46:50 +0500

    02

--- bin/while_loop02.dart
+++ bin/while_loop02.dart
@@ -1,5 +1,22 @@
 /*
   Create function func with "a " argument
   Return the number of even numbers up to "a".
-*/
-void main() {}
+*/int func(int a) {
+  int t = 0;
+  int c = 1;
+  while (a > c) {
+    c = c + 1;
+    if (c % 2 == 1) {
+      t += 1;
+    }
+    // a = a - 1;
+
+  }
+  return t;
+}
+
+void main() {
+  print(func(5));
+}                                               
+                         
+
