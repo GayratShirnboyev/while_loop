ba8cbd6cdaf7e59ca4ba876f96fae22c2d42c16a
G'ayrat <shirnboyevgayrat@gmail.com>
2022-10-27 11:28:54 +0500

    4

--- bin/while_loop04.dart
+++ bin/while_loop04.dart
@@ -2,4 +2,17 @@
   Create function func with "a " argument
   Return the sum of even numbers up to "a".
 */
-void main() {}
+int func(int a) {
+  int b = 0;
+  int c = 0;
+  while (b < a) {
+    b += 1;
+    if (b % 2 == 0) {
+      c += b;
+    }
+  }
+  return c;
+}
+void main() {
+  print(func(15));
+}
