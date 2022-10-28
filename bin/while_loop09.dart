2d51262dc8276625f34ac8128502dc1d343162e5
G'ayrat <shirnboyevgayrat@gmail.com>
2022-10-28 09:53:50 +0500

    9

--- bin/while_loop09.dart
+++ bin/while_loop09.dart
@@ -4,4 +4,19 @@
   
   
 */
-void main() {}
+String func(int a) {
+  int b = 0;
+  String v = '';
+  while (a > b) {
+    if (b % 2 == 0) {
+      v += b.toString();
+      
+    }
+    b += 1;
+  }
+  return v; // 13579
+}
+
+void main() {
+  print(func(10));
+}
